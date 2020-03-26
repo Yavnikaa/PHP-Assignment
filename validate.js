459025c33b9c303af6ecfd060555ab08605fdc55
Yavnikaa <56485761+Yavnikaa@users.noreply.github.com>
2020-03-26 17:24:54 +0530

    Update validate.js

--- validate.js
+++ validate.js
@@ -48,6 +48,11 @@ var q=document.getElementById("phn").value;
 
 }
 
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 
 $(document).ready(function(){
 
